5e33e18aae0206ea6c7565e11875ef208aa269f3
durotimi S. <138030174+durotimmi-sk@users.noreply.github.com>
2025-07-24 10:24:12 +0100

    utils.py

--- utils.py
+++ utils.py
@@ -7,19 +7,19 @@ from sentence_transformers import SentenceTransformer
 
 load_dotenv()
 
-class LocalEmbeddings(Embeddings):
-    def __init__(self):
-        self.model = SentenceTransformer("sentence-transformers/all-MiniLM-L12-v1")
+#class LocalEmbeddings(Embeddings):
+  #  def __init__(self):
+  #      self.model = SentenceTransformer("sentence-transformers/all-MiniLM-L12-v1")
     
-    def embed_query(self, text: str):
-        return self.model.encode(text).tolist()
+  #  def embed_query(self, text: str):
+   #     return self.model.encode(text).tolist()
     
-    def embed_documents(self, texts: list[str]):
-        return self.model.encode(texts).tolist()
+   # def embed_documents(self, texts: list[str]):
+    #    return self.model.encode(texts).tolist()
 
-embedding = LocalEmbeddings()
+#embedding = LocalEmbeddings()
 pc = pinecone.Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 index = pc.Index(os.getenv("PINECONE_INDEX"))
-vectorstore = PineconeVectorStore(index=index, embedding=embedding, namespace="loubby-navigation")
+vectorstore = PineconeVectorStore(index=index, embedding=None, namespace="loubby-navigation")
 
 print(f"Vectorstore initialized: {vectorstore is not None}")
